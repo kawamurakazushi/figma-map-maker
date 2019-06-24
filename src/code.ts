b862560d67879b17516619a6080a066b529aa3aa
Kazushi Kawamura <wakaka2277@gmail.com>
2019-06-24 23:58:42 +0900

    feat: generate map

--- src/code.ts
+++ src/code.ts
@@ -1,15 +1,31 @@
 figma.showUI(__html__);
 figma.ui.onmessage = msg => {
   if (msg.type === "image") {
-    const newImage = figma.createImage(msg.image);
-    const rect = figma.createRectangle();
-    rect.fills = [
-      {
-        type: "IMAGE",
-        image: newImage,
-        scaleMode: "CROP"
+    const selection = figma.currentPage.selection;
+    // TODO: more types
+    if (selection.length === 1) {
+      const node = selection[0];
+
+      console.log(node.type);
+
+      // TODO: id the type is PageNode
+      if (
+        node.type === "RECTANGLE" ||
+        node.type === "POLYGON" ||
+        node.type === "ELLIPSE"
+      ) {
+        const newImage = figma.createImage(msg.image);
+        node.fills = [
+          {
+            type: "IMAGE",
+            image: newImage,
+            scaleMode: "FILL"
+          }
+        ];
       }
-    ];
+      // const rect = figma.createRectangle();
+      // rect.resize(600, 300);
+    }
   }
   if (msg.type === "create-rectangles") {
     const nodes = [];
--- src/ui.tsx
+++ src/ui.tsx
@@ -1,27 +1,70 @@
-// document.getElementById("create").onclick = () => {
-//   const url =
-//     "https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key=AIzaSyCOHu6yxeJ1XAG6Rji_9j6kIaJVtUbrddk";
-
-//   const r = fetch(url).then(response =>
-//     response.arrayBuffer().then(buff => {
-//       parent.postMessage(
-//         { pluginMessage: { type: "image", image: new Uint8Array(buff) } },
-//         "*"
-//       );
-//     })
-//   );
-// };
-
-// document.getElementById("cancel").onclick = () => {
-//   parent.postMessage({ pluginMessage: { type: "cancel" } }, "*");
-// };
-
 import { h, render } from "preact";
 import { useState } from "preact/hooks";
 
-const App = () => {
-  const [count, setCount] = useState(0);
-  return <button onClick={() => setCount(count + 1)}>inc {count}</button>;
+interface MapOptions {
+  address: string;
+  type: string;
+  marker: boolean;
+}
+
+const send = ({ address, type, marker }: MapOptions) => {
+  const encodedAddress = encodeURIComponent(address);
+  const url =
+    `https://maps.googleapis.com/maps/api/staticmap?center=${encodedAddress}&zoom=13&size=600x300&maptype=${type}&key=AIzaSyCOHu6yxeJ1XAG6Rji_9j6kIaJVtUbrddk` +
+    (marker ? `&markers=color:red|${encodedAddress}` : "");
+
+  console.log(url);
+
+  fetch(url).then(response =>
+    response.arrayBuffer().then(buff => {
+      parent.postMessage(
+        { pluginMessage: { type: "image", image: new Uint8Array(buff) } },
+        "*"
+      );
+    })
+  );
 };
 
-render(<App name="cool working" />, document.getElementById("app"));
+const App = () => {
+  const [address, setAddress] = useState("");
+  const [type, setType] = useState("roadmap");
+  const [marker, setMarker] = useState(false);
+
+  const handleAddress = e => {
+    setAddress(e.target.value);
+  };
+
+  const handleType = e => {
+    setType(e.target.value);
+  };
+
+  const handleMarker = e => {
+    console.log(e.target.checked);
+    setMarker(e.target.checked);
+  };
+
+  return (
+    <div>
+      <div>
+        <input value={address} onInput={handleAddress} />
+      </div>
+      <div>
+        <select onChange={handleType} value={type}>
+          {["roadmap", "satellite", "hybrid", "terrain"].map(t => (
+            <option key={t} value={t}>
+              {t}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div>
+        <input onChange={handleMarker} type="checkbox">
+          Display Marker
+        </input>
+      </div>
+      <button onClick={() => send({ address, type, marker })}>Send</button>
+    </div>
+  );
+};
+
+render(<App />, document.getElementById("app"));
